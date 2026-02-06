9ded5249d4d0c4efff6b19e9f2438441a3233f5d
yancheng69 <yanchengl3@nycstudents.net>
2026-02-06 14:19:25 +0000

    Update

--- WDLesson4_2Practical/script.js
+++ WDLesson4_2Practical/script.js
@@ -2,7 +2,7 @@
 /*
 function name(){
   // Step 1: Create variables for the information from the text inputs and the output element
-
+   
   // Step 2: Complete the process
   //    a) Perform any calculations necessary
   //    b) Perform necessary decisions
@@ -11,6 +11,19 @@ function name(){
 
 }
 */
+function area() {
+  let length = document.getElementById("length").value;
+  let width = document.getElementById("width").value;
+  let output = document.getElementById("output");
+
+  if (length <= 0 || width <= 0) {
+    output.innerHTML = "Inappropriate measurement";
+  } else {
+    let area = length * width;
+    output.innerHTML = "Area = " + area;
+  }
+}
+
 
 /* Challenge 2:  Create the function for Challenge 1 scenario */
 
@@ -21,5 +34,30 @@ function name(){
 2) You will need to write HTML as part of the output.  
    For example, output.innerHTML = `<input src="pic.png">` would place the image in the <div>*/
 function pet(){
+ let animal = document.getElementById("animal").value;
+  let emotion = document.getElementById("emotion").value;
+  let output = document.getElementById("output");
+  let img = "";
+
+  // Step 2: Decision logic
+  if (animal === "Bear" && emotion === "Funny") {
+    img = "funnyBear.jpg";
+  } 
+   if (animal === "Bear" && emotion === "Sad") {
+    img = "sadBear.jpg";
+  } 
+   if (animal === "Cat" && emotion === "Funny") {
+    img = "funnyCat.jpg";
+  } 
+     if (animal === "Cat" && emotion === "Sad") {
+    img = "sadCat.jpg";
+  } 
+   if (animal === "Dog" && emotion === "Funny") {
+    img = "funnyDog.jpg";
+  } 
+   if (animal === "Dog" && emotion === "Sad") {
+    img = "sadDog.jpg";
+  }
+    output.innerHTML = `<img src="${img}" >`;
 
 }
