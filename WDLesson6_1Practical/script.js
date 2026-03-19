b85b3f800dee6f7a0a34858502ff258287b20344
yancheng69 <yanchengl3@nycstudents.net>
2026-03-19 12:26:54 +0000

    update

---
+++ WDLesson6_1Practical/script.js
@@ -0,0 +1,34 @@
+/*
+  List of all the images. "beeftips.jpg","brisket.jpg","ribeye.jpg","steak.jpg","clams.jpg","crabs.jpg","lobster.png","scallops.jpg","shrimp.jpg","tuna.jpg","flan.jpg","passionberry.jpeg","oreocup.jpg"
+*/ 
+//Challenge 1: Add the appropriate images to each array below from the list above.
+//Challenge 2: Add appropriate prices for each food in their corresponding array.
+//Challenge 3: Add appropriate titles for each food in their corresponding array.  Use the image names as a hint for the food title.
+let meat_titles = [];
+let meats = [];
+let meat_prices = [];
+let seafood_titles = [];
+let seafood = [];
+let seafood_prices = [];
+let dessert_titles = [];
+let dessert = [];
+let dessert_prices = [];
+
+function init(){
+  //Each food has its own output container
+  let m = document.getElementById("meats");
+  let s = document.getElementById("seafoods");
+  let d = document.getElementById("desserts");
+  let build = ``;
+  //Challenge 4:  Build cards for the meats. Place the build in the meat container. 
+
+  //Challenge 5:  Build cards for the seafoods. Place the build in the seafood container.
+
+  //Challenge 6:  Build cards for the desserts. Place the build in the dessert container.
+
+  /*Notes for Challenges 4 through 6:
+      1) Each challenge requires its own for loop to build the output.
+      2) You can reuse the build variable as long as you set it back to `` before each loop.
+      3) Create a basic card with the title, image and price.
+  */
+}
