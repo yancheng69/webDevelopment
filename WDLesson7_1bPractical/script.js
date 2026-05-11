02fbadebf2a9049840b7178152b2faa475964057
yancheng69 <yanchengl3@nycstudents.net>
2026-05-11 13:14:33 +0000

    up

--- WDLesson7_1bPractical/script.js
+++ WDLesson7_1bPractical/script.js
@@ -1,17 +1,24 @@
 //Data Source: https://data.cityofnewyork.us/City-Government/Open-Parking-and-Camera-Violations/nc67-uf89
 
 let data, output, result;
-function init(){
-  $.ajaxSetup({async: false});
-  data = $.getJSON("data.js").responseJSON;
-
+async function init(){
+let link = "data.js"
+info = await fetch(link);
+data = await info.json();
   output = get("output");
+  
+let build ="";
 
   //Challenge 2: Test the card( ) developed in Challenge 1 by displaying the first violation to the webpage.
-  output.innerHTML = card(data[0]);
+  for(let i = 0; i < data.length; i+=1){
+let violation = data[i];
+build += card(violation);
+}
+
+output.innerHTML = build;
 
   //Challenge 4: Display all the Open Parking and Camera Violations to the web page using the cards( ) from Challenge 3.
-   output.innerHTML = cards(data);
+   
   
 }
 
